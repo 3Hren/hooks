95ca7cca2250ec66783d7e14996283ee16dac2ae
Evgeny Safronov <division494@gmail.com>
2015-10-20 01:09:13 +0300

    fix(root): fix random crashes. This is very very long commit
    This is loggg dfdf dfsfsd rewko fdfd fdsfsfd mmmfdt dadsdcr sdscd dsdsf rrsrrsr sdad.

--- server.rb
+++ server.rb
@@ -10,7 +10,8 @@ end
 
 post '/' do
   @payload = JSON.parse(params[:payload])
-  puts :payload => @payload
+
+  # puts JSON.pretty_generate(@payload)
 
   case request.env['HTTP_X_GITHUB_EVENT']
   when "pull_request"
@@ -32,19 +33,23 @@ helpers do
     repo = pull['base']['repo']['full_name']
     hash = pull['head']['sha']
 
+    @client.create_status(repo, hash, 'pending', :context => 'gittools/limits/subject', :description => 'The GitTools check is pending ...')
     @client.create_status(repo, hash, 'pending', :context => 'gittools', :description => 'The GitTools check is pending ...')
 
-    commits = @client.pull_commits repo, request[:number]
+    commits = @client.pull_request_commits(repo, pull['number'])
+    puts JSON.pretty_generate(commits)
+
     commits.each do |commit|
-      if commit[:commit][:message].len >= 50
+      if commit['commit']['message'].length >= limits[:subject]
         raise Exception.new 'the subject line must fit in 50 characters'
       end
     end
 
-  rescue Exception => e
-    puts :error => e
-    # @client.create_status(repo, hash, 'error', :context => 'gittools', :description => "The GitTools check failed: #{e}")
-  ensure
+    @client.create_status(repo, hash, 'success', :context => 'gittools/limits/subject', :description => 'The GitTools check passed')
+
+    # Do another work.
     @client.create_status(repo, hash, 'success', :context => 'gittools', :description => 'The GitTools check passed')
+  rescue Exception => e
+    @client.create_status(repo, hash, 'error', :context => 'gittools', :description => "The GitTools check failed")
   end
 end
